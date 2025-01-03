f54350e9c854bb441287e6c1b90279d35b693472
Ab <52878876+Dulchik@users.noreply.github.com>
2025-01-03 09:59:32 +0100

    main design and app structure

--- app/page.js
+++ app/page.js
@@ -1,11 +1,33 @@
+"use client";
+
 export default function Home() {
+  const handleSearch = () => {
+    alert("your mom");
+  };
+
   return (
-    <main>
+    <main className="max-w-md mx-auto">
       {/* HEADER */}
-      <section></section>
+      <section>
+        <div className="flex  justify-center my-10">
+          <h1 className="text-3xl font-bold">WATCHLIST</h1>
+        </div>
+      </section>
 
       {/* CONTENT */}
-      <section></section>
+      <section>
+        <div className="space-x-5">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <button className="btn px-7" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+        <div></div>
+      </section>
 
       {/* FOOTER */}
       <section></section>
